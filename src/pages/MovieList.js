4d9e98db1aa65996c2729dfe627bd85c6dc3643d
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-05-31 21:51:51 -0300

    Requisito2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,14 +10,22 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      load: false,
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => {
+      const load = true;
+      this.setState({ movies, load });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, load } = this.state;
+    if (!load) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
