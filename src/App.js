09f04506edf2c1fa91f4ab1d63eee2ea80794a91
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-05-31 21:33:06 -0300

    Corrigindo lint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MovieDetails, MovieList, NewMovie, EditMovie,NotFound } from './pages'
+import { MovieDetails, MovieList, NewMovie, EditMovie, NotFound } from './pages';
+
 function App() {
-  return (  
+  return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <switch>
-      <Route exact path="/" component={ MovieList } />
-      <Route exact path="/movies/:id" component={ MovieDetails } />
-      <Route exact path="/movies/new" component={ NewMovie } />
-      <Route exact path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="/" component={ NotFound } />
-      </switch>
+      <Switch>
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />      
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/" component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
